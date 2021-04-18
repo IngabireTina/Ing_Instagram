12d9e09edd3a2346521f31d352fa2ee4be110f91
IngabireTina <ingabire067@gmail.com>
2021-04-18 16:59:50 -0400

    added comment update user forms

--- app/forms.py
+++ app/forms.py
@@ -15,3 +15,18 @@ class SignUpForm(UserCreationForm):
         model = User
         fields = ('username', 'password1', 'password2')
 
+class UpdateUserProfileForm(forms.ModelForm):
+
+    class Meta:
+        model = Profile
+        fields = ['profile', 'bio']
+
+class CommentForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super()__init__(*args, **kwargs)
+        self.fields['comment'].widget = forms.TextInput()
+        self.fields['comment'].widget.attrs['placeholder'] = 'Add a comment'
+
+    class Meta:
+        model = Comment
+        fields = ('comment', )
