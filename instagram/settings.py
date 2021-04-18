30fec3f3ac9916fbfc49f9574f537664b3b7c117
IngabireTina <ingabire067@gmail.com>
2021-04-18 13:21:34 -0400

    edited settings added media url

--- instagram/settings.py
+++ instagram/settings.py
@@ -130,7 +130,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'stacticfiles')
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
+
+# LOGIN_REDIRECT_URL = 'home'
+
+# LOGOUT_REDIRECT_IRL = 'home'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
