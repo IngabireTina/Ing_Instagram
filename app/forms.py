582cb934ed6862c3c078c72cb5a780faada8ce80
IngabireTina <ingabire067@gmail.com>
2021-04-18 16:37:38 -0400

    added post and signup form

---
+++ app/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import *
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        models = Post
+        fields = ('image', 'name', 'caption')
+
+class SignUpForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2')
+
